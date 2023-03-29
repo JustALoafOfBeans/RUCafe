1c528b0aaaa8dd1ed319091080847defbb79fae2
JustALoafOfBeans <victoriachen4520@gmail.com>
2023-03-29 00:10:07 -0400

    rename and refactor

--- src/main/java/com/example/rucafe/CafeController.java
+++ src/main/java/com/example/rucafe/CafeController.java
@@ -3,7 +3,7 @@ package com.example.rucafe;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class CafeController {
     @FXML
     private Label welcomeText;
 
