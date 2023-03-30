8b097731705baed4f2025df16c7d26651414d782
Victoria Chen <victoriachen4520@gmail.com>
2023-03-29 20:09:54 -0400

    donut view done?

--- src/main/java/com/example/rucafe/CafeController.java
+++ src/main/java/com/example/rucafe/CafeController.java
@@ -12,14 +12,15 @@ import java.io.IOException;
 public class CafeController {
     @FXML
     protected void displayDonutsViewer() {
-        Stage view1 = new Stage();
+        Stage stage = new Stage();
         BorderPane root;
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("donut-view.fxml"));
             root = (BorderPane) loader.load();
             Scene scene = new Scene(root, 500, 500);
-            view1.setScene(scene);
-            view1.show();
+            stage.setTitle("Donut Select");
+            stage.setScene(scene);
+            stage.show();
             DonutController donutPage = loader.getController();
             donutPage.setMainController(this);
         } catch (IOException e) {
@@ -33,14 +34,15 @@ public class CafeController {
 
     @FXML
     protected void displayCoffeeViewer() {
-        Stage view1 = new Stage();
+        Stage stage = new Stage();
         BorderPane root;
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("coffee-view.fxml"));
             root = (BorderPane) loader.load();
             Scene scene = new Scene(root, 500, 500);
-            view1.setScene(scene);
-            view1.show();
+            stage.setTitle("Coffee Select");
+            stage.setScene(scene);
+            stage.show();
             CoffeeController coffeePage = loader.getController();
             coffeePage.setMainController(this);
         } catch (IOException e) {
@@ -54,14 +56,15 @@ public class CafeController {
 
     @FXML
     protected void displayBasketViewer() {
-        Stage view1 = new Stage();
+        Stage stage = new Stage();
         BorderPane root;
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("basket-view.fxml"));
             root = (BorderPane) loader.load();
             Scene scene = new Scene(root, 500, 500);
-            view1.setScene(scene);
-            view1.show();
+            stage.setTitle("Basket");
+            stage.setScene(scene);
+            stage.show();
             BasketController basketPage = loader.getController();
             basketPage.setMainController(this);
         } catch (IOException e) {
@@ -75,14 +78,15 @@ public class CafeController {
 
     @FXML
     protected void displayAllOrdersViewer() {
-        Stage view1 = new Stage();
+        Stage stage = new Stage();
         BorderPane root;
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("allorders-view.fxml"));
             root = (BorderPane) loader.load();
             Scene scene = new Scene(root, 500, 500);
-            view1.setScene(scene);
-            view1.show();
+            stage.setTitle("All Store Orders");
+            stage.setScene(scene);
+            stage.show();
             AllOrdersController allordersPage = loader.getController();
             allordersPage.setMainController(this);
         } catch (IOException e) {
