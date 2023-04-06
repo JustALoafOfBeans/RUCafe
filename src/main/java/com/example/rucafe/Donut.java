166e7e4aaf9dc9cc1681c02923f1f0c42f83d730
brizbird14 <brizbird14@gmail.com>
2023-04-06 17:54:56 -0400

    Menu, Coffee, Donut comments

--- src/main/java/com/example/rucafe/Donut.java
+++ src/main/java/com/example/rucafe/Donut.java
@@ -5,10 +5,23 @@ import javafx.scene.control.ComboBox;
 
 import java.text.DecimalFormat;
 
+/**
+ * The Donut class extends the abstract MenuItem class and describes orders
+ * of donuts from the RU Cafe
+ * @author Victoria Chen
+ */
 public class Donut extends MenuItem{
-
+    /**
+     * Type of donut item selected (cake, yeast, or hole)
+     */
     private String type;
+    /**
+     * Amount of item selected
+     */
     private int quantity;
+    /**
+     * Flavor of donut selected, options vary with type
+     */
     private String flavor;
     /**
      * Use DF.format(value) to round value to two decimals places. Rounding
@@ -16,12 +29,22 @@ public class Donut extends MenuItem{
      */
     private static final DecimalFormat DF = new DecimalFormat("0.00");
 
+    /**
+     * Constructor for Donut class
+     * @param type type of donut treat String
+     * @param fla flavor of donut as a String
+     * @param num quantity of donuts added
+     */
     public Donut(String type, String fla, int num) {
         this.type = type;
         this.quantity = num;
         this.flavor = fla;
     }
 
+    /**
+     * Method that returns price of added item based on quantity, flavor, and type
+     * @return Price as a double, formatted to two decimal places
+     */
     public double itemPrice() {
         switch (type)  {
             case "Yeast Donut":
@@ -35,6 +58,11 @@ public class Donut extends MenuItem{
         }
     }
 
+    /**
+     * Checks if a given Donut object is equivalent to the current one
+     * @param equalObject Donut to compare
+     * @return true if Donut objects are equivalent, false otherwise
+     */
     @Override
     public boolean equals(Object equalObject) {
         if (equalObject instanceof Donut)
@@ -45,6 +73,10 @@ public class Donut extends MenuItem{
         return false; // Not of type student, invalid comparison
     }
 
+    /**
+     * Compares two Donut objects
+     * @return 0 if String forms equal, -1 if not
+     */
     public int compareTo(Donut compareDonut) {
         if (this.toString().equals(compareDonut.toString())) {
             return 0;
@@ -52,23 +84,43 @@ public class Donut extends MenuItem{
         return -1;
     }
 
+    /**
+     * Method that presents Donut object as a printable String format
+     * @return String format of Donut
+     */
     @Override
     public String toString() {
         return quantity + " " + type + " (" + flavor + ")";
     }
 
+    /**
+     * Method that returns type of Donut
+     * @return type as String
+     */
     public String getType() {
         return type;
     }
 
+    /**
+     * Method that returns flavor of Donut object
+     * @return flavor as String
+     */
     public String getFlavor() {
         return flavor;
     }
 
+    /**
+     * Method that returns quantity of Donut object
+     * @return number as int
+     */
     public int getQuantity() {
         return quantity;
     }
 
+    /**
+     * Method that updates quantity of Donut object
+     * Used to account for duplicate orders (same type and flavor)
+     */
     public void setQuantity(int newQuantity) {
         quantity = newQuantity;
     }
--- src/main/java/com/example/rucafe/MenuItem.java
+++ src/main/java/com/example/rucafe/MenuItem.java
@@ -1,10 +1,21 @@
 package com.example.rucafe;
 
-
+/**
+ * The MenuItem class is an abstract class that includes methods to define
+ * items that could be added to an order from the RU cafe.
+ */
 public abstract class MenuItem {
 
+    /**
+     * Abstract method that returns the price of an item as a double
+     * @return price as a formatted double
+     */
     public abstract double itemPrice();
 
+    /**
+     * Abstract method that returns menu item as a string
+     * @return String format of item
+     */
     @Override
     public abstract String toString();
 
