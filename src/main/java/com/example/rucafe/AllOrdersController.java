51cf260903f362fb35f179737098df2bd4b55acc
Victoria Chen <victoriachen4520@gmail.com>
2023-04-06 18:08:47 -0400

    able to export orders to .txt

--- src/main/java/com/example/rucafe/AllOrdersController.java
+++ src/main/java/com/example/rucafe/AllOrdersController.java
@@ -10,6 +10,9 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.ListView;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class AllOrdersController {
@@ -54,20 +57,45 @@ public class AllOrdersController {
         ordersView.refresh();
     }
 
-    /**
-     * uhhhh im tired so im not sure what im doing rn. this adds an order to
-     * the list of all orders for the store but idk how to do this >.>
-     * @param content
-     */
-    @FXML
-    protected void addToStoreOrder(ObservableList<MenuItem> content) {
-        orders.add(new Order(nextOrderNum, content));
-        nextOrderNum++;
-    }
-
     @FXML
     protected void exportOrders() {
-        //todo export orders to a txt
+        try {
+            // file will be created in the RUCafe folder
+            File target = new File("exportedOrders.txt");
+            if (target.createNewFile()) {
+                writeOrdersToFile(target.getName());
+            } else {
+                writeOrdersToFile(target.getName());
+            }
+        } catch (IOException e) {
+            // System.out.println("An error occurred. Did not output to a file");
+        }
+    }
+
+    private void writeOrdersToFile(String fileName) {
+        try {
+            int num = 0;
+            orders = mainController.getAllordersList();
+            FileWriter myWriter = new FileWriter(fileName);
+            if (orders != null) {
+                for (Order ord : orders) {
+                    num = ord.getNum();
+                    myWriter.write("Order #" + num + "\n");
+                    selectedOrder = ord.getItems();
+                    if (selectedOrder != null) {
+                        for (MenuItem item : selectedOrder) {
+                            myWriter.write(item.toString() + "\n");
+                        }
+                    }
+                    myWriter.write("\n");
+                }
+            } else {
+                myWriter.write("No orders to export.");
+            }
+            myWriter.close();
+        } catch (IOException e) {
+            // System.out.println("An error occurred. Unable to write.");
+        }
     }
 
     //Get the reference to the MainController object
