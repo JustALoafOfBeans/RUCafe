fcffbfba95a340aed604fd68fe754b775b6f56d7
brizbird14 <brizbird14@gmail.com>
2023-04-06 06:20:04 -0400

    Bakset price

--- src/main/java/com/example/rucafe/BasketController.java
+++ src/main/java/com/example/rucafe/BasketController.java
@@ -5,6 +5,8 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import java.text.DecimalFormat;
 
 public class BasketController {
 
@@ -12,14 +14,20 @@ public class BasketController {
     public ListView<MenuItem> order;
     @FXML
     public Button removeButton, clearButton, finalButton;
+    @FXML
+    public TextField basketSubtotal, basketTax, basketTotal;
     ObservableList<MenuItem> menuList;
     private CafeController mainController;
+    private static final DecimalFormat DF = new DecimalFormat("0.00");
+
+    private static final double taxNJ = 0.06625;
     //Get the reference to the MainController object
 
     public void setMainController (CafeController controller) {
         mainController = controller;
         menuList = mainController.returnBasket();
         order.setItems(menuList);
+        updateTotals();
     }
 
     @FXML
@@ -28,14 +36,26 @@ public class BasketController {
         mainController.removeItem(selected);
         menuList.remove(selected);
         order.refresh();
-        for (MenuItem item : menuList) {
-            System.out.println(item);
-        }
-        System.out.println();
+        updateTotals();
     }
 
     @FXML
     protected void onClearOrder() {
-        // todo
+        // todo i'm teired
+        updateTotals();
+    }
+
+    protected void updateTotals () {
+        double subtotal = 0;
+        for (int i = 0; i < order.getItems().size(); i++) {
+            subtotal += (order.getItems().get(i).itemPrice());
+        }
+        basketSubtotal.setText("$ " + Double.valueOf(DF.format(subtotal)));
+
+        double tax = subtotal * taxNJ;
+        basketTax.setText("$ " + Double.valueOf(DF.format(tax)));
+
+        double total = subtotal + tax;
+        basketTotal.setText("$ " + Double.valueOf(DF.format(total)));
     }
 }
